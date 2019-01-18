b8614f33a73bba33527dbfb17102a7c4a724d2c0
Vladislav <klychik-nsk54rus@mail.ru>
2019-01-18 10:30:45 +0700

    18 частиц фортран

--- Untitled.m
+++ Untitled.m
@@ -1,7 +1,9 @@
 clear;    
-
-ss1=load('output.txt');
+ss1=load('Displacment_X_top_right.txt');
+ss2=load('Displacment_Y_top_right.txt');
+ss3=load('output.txt');
 scatter(ss1(:,1),ss1(:,2));
+plot( ss3(:,3), ss3(:,1), ss3(:,3), ss3(:,2),ss1(:,2),ss1(:,3),'--',ss2(:,2),ss2(:,3),'--');
 
 
 
