c484bff203f0d4e00d34958ade7d8ab0402b7173
Vladislav <klychik-nsk54rus@mail.ru>
2019-03-13 18:20:17 +0700

    сравнение двух подходов по перемещениям
    1

--- Untitled.m
+++ Untitled.m
@@ -1,21 +1,21 @@
 clear;   
 
-ss1=load('x.txt');
-ss2=load('y.txt');
+ss1=load('Displacment_X_top_right1.txt');
+ss2=load('Displacment_Y_top_right1.txt');
 ss3=load('C11.txt');
 ss4=load('C22.txt');
 
 ss7=load('Cauchy_stresses_1_1_MIDDLE_SPECTACULAR_lamped_mass.txt');
 ss8=load('Cauchy_stresses_1_2_MIDDLE_SPECTACULAR_lamped_mass.txt');
 ss9=load('Cauchy_stresses_2_2_MIDDLE_SPECTACULAR_lamped_mass.txt');
-
+ss10=load('output_x_21_r_8.txt');
 ss5=load('output_C.txt');
 ss6=load('output_x.txt');
 
 scatter(ss1(:,1),ss1(:,2));
-%plot( ss6(:,3), ss6(:,1),'r', ss6(:,3), ss6(:,2),'r',ss1(:,2), ss1(:,3),'k', ss2(:,2), ss2(:,3),'k');
+plot( ss10(:,3), ss10(:,1),'b', ss10(:,3), ss10(:,2),'b',ss6(:,3), ss6(:,1),'r', ss6(:,3), ss6(:,2),'r',ss1(:,2), ss1(:,3),'k', ss2(:,2), ss2(:,3),'k');
 %plot( ss5(:,4), ss5(:,1),ss5(:,4), ss5(:,3),ss5(:,4),ss5(:,2),ss3(:,2), ss3(:,3),'k',ss4(:,2), ss4(:,3),'k');
-plot( ss5(:,4), ss5(:,1),'r',ss5(:,4), ss5(:,3),'r',ss5(:,4),ss5(:,2),'r',ss7(:,2), ss7(:,3),'k',ss8(:,2), ss8(:,3),'k',ss9(:,2), ss9(:,3),'k');
+%plot( ss5(:,4), ss5(:,1),'r',ss5(:,4), ss5(:,3),'r',ss5(:,4),ss5(:,2),'r',ss7(:,2), ss7(:,3),'k',ss8(:,2), ss8(:,3),'k',ss9(:,2), ss9(:,3),'k');
 
 
 
